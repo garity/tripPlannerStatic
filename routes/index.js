ba9e709d978d9343feabd0a367deecadabc3018f
Garity Chapman <garitychapman@gmail.com>
2016-11-18 16:25:16 -0500

    got first route working

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,42 @@
 const express = require('express');
 const router = express.Router();
 module.exports = router;
+const Promise = require('bluebird');
+const Models = require('../models');
+const Activity = Models.Activity;
+const Hotel	= Models.Hotel;
+const Restaurant = Models.Restaurant;
 
 router.get('/', function (req, res, next) {
-    res.render('index');
+	const all = Promise.all([
+		Hotel.findAll({}),
+		Restaurant.findAll({}),
+		Activity.findAll({})
+		])
+	.then(function(items){
+		res.render('index', {
+	    hotels: items[0],
+	    restaurants: items[1],
+	    activities: items[2],
+	  });
+	});
+ //    var outerScopeContainer = {};
+	// Hotel.findAll()
+	// .then(function (dbHotels) {
+	//   outerScopeContainer.dbHotels = dbHotels;
+	//   return Restaurant.findAll();
+	// })
+	// .then(function (dbRestaurants) {
+	//   outerScopeContainer.dbRestaurants = dbRestaurants;
+	//   return Activity.findAll();
+	// })
+	// .then(function (dbActivities) {
+	//   res.render('index', {
+	//     hotels: outerScopeContainer.dbHotels,
+	//     restaurants: outerScopeContainer.dbRestaurants,
+	//     activities: dbActivities
+	//   });
+	// })
+	// .catch(next);
+    
 });
